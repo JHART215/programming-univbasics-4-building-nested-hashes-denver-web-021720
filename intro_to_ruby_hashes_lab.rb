eb03ce9b0a1bb98563fc478d01af69b14ac37860
Jeremy Hart <jerome215@comcast.net>
2020-01-31 16:34:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,15 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{:oil => {}}
+	{:railroads => {}}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-  {:oil => {:fields => 4}}
+  {:railroads => {:pieces => 4}}
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-  {:oil => {:fields => 4, :rent_in_dollars => {:one_field_owned => 50, :two_fields_owned => 100, :three_fields_owned => 150, :four_fields_owned => 200}, :monthly_output => {:first_field => {}, :second_field => {}, :third_field => {}, :forth_field => {}}}}
+  {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {}}}}
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-  {:oil => {:fields => 4, :rent_in_dollars => {:one_field_owned => 50, :two_fields_owned => 100, :three_fields_owned => 150, :four_fields_owned => 200}, :monthly_output => {:first_field => {'$500'}, :second_field => {'$1000'}, :third_field => {'$750'}, :forth_field => {'$364'}}}}
+  {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}, :names => {:reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad => {"mortgage_value" => "$400"}, :shortline_railroad => {"mortgage_value" => "$800"}}}}
 end
